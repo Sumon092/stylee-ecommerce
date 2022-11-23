4a37bb5969c349d1d99e903a47827434b7c9c9be
SUMON <m.sumon@outlook.com>
2022-11-24 05:36:06 +0600

    footer completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Facebook, Instagram, Twitter, YouTube } from '@mui/icons-material';
+import { Facebook, Instagram, MailOutline, PhoneAndroid, Room, Twitter, YouTube } from '@mui/icons-material';
 
 const Container = styled.div`
 display:flex;
@@ -26,13 +26,44 @@ height:40px;
 width:40px;
 border-radius:50%;
 color:white;
+background-color:#${props => props.color};
+display:flex;
+justify-content:center;
+align-items:center;
+margin-right:20px;
+cursor:pointer;
 `
 const Center = styled.div`
 flex:1;
+padding:20px;
+`
+const Title = styled.h3`
+margin-bottom:30px;
+`
+const List = styled.ul`
+margin:0;
+padding:0;
+list-style:none;
+display:flex;
+flex-wrap:wrap;
+`
+const ListItem = styled.li`
+width:50%;
+margin-bottom:10px;
 `
 const Right = styled.div`
 flex:1;
+padding:20px;
 `
+const ContactItem = styled.div`
+display:flex;
+align-items:center;
+margin-bottom:10px;
+`
+const Payment = styled.img`
+    width: 50%;
+    cursor:pointer;
+`;
 
 const Footer = () => {
     return (
@@ -47,7 +78,7 @@ const Footer = () => {
                     <SocialIcon color='E4405F'>
                         <Instagram />
                     </SocialIcon>
-                    <SocialIcon color='red'>
+                    <SocialIcon color='FF0000'>
                         <YouTube />
                     </SocialIcon>
                     <SocialIcon color='55ACEE'>
@@ -55,8 +86,28 @@ const Footer = () => {
                     </SocialIcon>
                 </SocialContainer>
             </Left>
-            <Center></Center>
-            <Right></Right>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Men Fashion</ListItem>
+                    <ListItem>Women Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms & Conditions</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><Room style={{ marginRight: '10px' }} />555, South gate, Sherpur 2100</ContactItem>
+                <ContactItem><PhoneAndroid style={{ marginRight: '10px' }} />+880 123456789</ContactItem>
+                <ContactItem><MailOutline style={{ marginRight: '10px' }} />contact@sumon.dev</ContactItem>
+
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
         </Container>
     );
 };
